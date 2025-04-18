825429fdbba57093052f677b45cc8d5ec80241c5
peng <pengcheng@purdue.edu>
2025-04-18 07:04:45 +0000

    update the script to sync data

--- results_sync.sh
+++ results_sync.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
-rsync --progress -azhe 'ssh -i ~/.ssh/mobisys2025.pem' /home/data/agile3d/carl/experiments/ agile3d@172.30.53.226:~/fastdata/agile3d/agile3d/carl/experiments/
+rsync --progress -azhe 'ssh -i ~/.ssh/mobisys2025.pem' /home/data/agile3d/output/exp1/ agile3d@172.30.166.233:/home/pengcheng/data/agile3d/output/exp1/
+rsync --progress -azhe 'ssh -i ~/.ssh/mobisys2025.pem' /home/data/agile3d/output/exp3/ agile3d@172.30.166.233:/home/pengcheng/data/agile3d/output/exp3/
+rsync --progress -azhe 'ssh -i ~/.ssh/mobisys2025.pem' agile3d@172.30.166.233:/home/pengcheng/data/agile3d/output/exp1/ /home/data/agile3d/output/exp1/
+rsync --progress -azhe 'ssh -i ~/.ssh/mobisys2025.pem' agile3d@172.30.166.233:/home/pengcheng/data/agile3d/output/exp3/ /home/data/agile3d/output/exp3/
