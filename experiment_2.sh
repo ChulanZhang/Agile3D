dab54ec9705d24b1b15a7504655ef7bce356a24c
pengcheng <wang4495@purdue.edu>
2025-04-18 19:31:22 +0000

    update exp scripts

--- experiment_2.sh
+++ experiment_2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd tools
+cd /home/data/agile3d/tools
 
 export PYTHONWARNINGS=ignore::UserWarning:pcdet.ops.iou3d_nms.iou3d_nms_utils
 
--- experiment_3.sh
+++ experiment_3.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # Not necessary to run this script, the results are saved on the evaluation server
-cd tools
+cd /home/data/agile3d/tools
 
 python pareto_frontier.py
